a7a4447e82f0ff1e7428ace091ac0aefeb121b5f
Divyansh <srivas.divyansh22@gmail.com>
2023-04-25 02:34:46 +0530

    Added the function of voting by the existing user

--- backend/contracts/Forum.sol
+++ backend/contracts/Forum.sol
@@ -5,10 +5,35 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Forum {
-    
-    constructor()  {
-        
+    constructor() {}
+
+    //Created the struct for the user
+    struct User {
+        string name;
+        bool isVoted;
     }
 
-   
+    //Created the struct for new user
+    struct NewUser {
+        string name;
+        uint votecount;
+    }
+
+    //Mapped the address with the existing user
+    mapping (address => User) user;
+
+    //Mapped the address with the new user
+    mapping (address => NewUser) newuser;
+
+    //Voting function
+    //It takes the address of the existing user
+    //and new user
+    function vote(address _add, address _newadd) public {
+        //Checks whether the existing user voted
+        require(!(user[_add].isVoted));
+        //Increases the vote by 1
+        newuser[_newadd].votecount++;
+        //Marking that the existing user has already voted
+        user[_add].isVoted = true;
+    }
 }
