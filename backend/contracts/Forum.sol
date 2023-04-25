7436166d2624780e5e794b26b8460f2f92030e06
Mitali Dixit <mitali.dixit04@gmail.com>
2023-04-25 15:51:10 +0530

    first commit

--- backend/contracts/Forum.sol
+++ backend/contracts/Forum.sol
@@ -9,6 +9,25 @@ contract Forum {
     constructor()  {
         
     }
+//array to store the addresses of users who have requested entry
+address[] public requests;
+
+//mapping to keep track whether user has already requested entry
+mapping(address => bool) public hasRequested;
+
+//mapping to keep track of existing users
+mapping (address => bool) public isExistingUser;
+
+function requestEntry() public {
+    //check if user has already requested to join
+    require(!hasRequested[msg.sender], "User has already requested to join");
+    //check if user is already a member 
+    require(!isExistingUser[msg.sender], "User is already a member");
+    //address of new unaccepted user is added to requests array
+    requests.push(msg.sender);
+    //marking the entry request
+    hasRequested[msg.sender] = true;
+}
 
    
 }
