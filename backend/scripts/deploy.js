faaecbcf98adcafc9fb0060cb284f181346bb036
Nikita Kumari <mailnikita2808@gmail.com>
2023-04-26 22:22:41 +0530

    Changed Deploy

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -7,26 +7,19 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Forum = await hre.ethers.getContractFactory("Forum")
+  const forum = await Forum.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await forum.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Factory deployed to:", forum.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+     .then(() => process.exit(0))
+     .catch((error) => {
+        console.log(error);
+        process.exit(1);
+     });
