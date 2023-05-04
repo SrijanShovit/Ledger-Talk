0283d35e2cdee7005d70d8f0087602f34fd3763a
Divyansh <srivas.divyansh22@gmail.com>
2023-05-04 22:27:32 +0530

    Deleted the struct and mapped the address of user and new user to their property.

--- backend/contracts/Forum.sol
+++ backend/contracts/Forum.sol
@@ -7,33 +7,23 @@ pragma solidity ^0.8.9;
 contract Forum {
     constructor() {}
 
-    //Created the struct for the user
-    struct User {
-        string name;
-        bool isVoted;
-    }
+    
 
-    //Created the struct for new user
-    struct NewUser {
-        string name;
-        uint votecount;
-    }
+    //Mapped the address with the boolean whether the user has voted or not.
+    mapping (address => bool) user_isVoted;
 
-    //Mapped the address with the existing user
-    mapping (address => User) user;
-
-    //Mapped the address with the new user
-    mapping (address => NewUser) newuser;
+    //Mapped the address with the votecount
+    mapping (address => uint) newuser_votecount;
 
     //Voting function
     //It takes the address of the existing user
     //and new user
     function vote(address _add, address _newadd) public {
         //Checks whether the existing user voted
-        require(!(user[_add].isVoted));
+        require(!(user[_add]));
         //Increases the vote by 1
-        newuser[_newadd].votecount++;
+        newuser[_newadd]++;
         //Marking that the existing user has already voted
-        user[_add].isVoted = true;
+        user[_add] = true;
     }
 }
